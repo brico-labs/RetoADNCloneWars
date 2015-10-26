67dfcb9dd9da4e6af1a4179f24fd7ad3761ed786
Rafa Couto <caligari@treboada.net>
2015-10-26 13:06:10 +0100

    Cron job.

--- src/adn-cw.sh
+++ src/adn-cw.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-SRC='adn-cw.scad'
-DIR='../stl'
+# paths
+BASEDIR=$(dirname $0)
+OPENSCAD=/usr/bin/openscad
 
+# parameters and defaults
+SRC=${1-$BASEDIR/adn-cw.scad}
+DIR=${2-$BASEDIR/../stl}
+
+# OpenSCAD is installed
 VER=$($OPENSCAD --version 2>&1)
-
-[ $? -ne 1 ] && ( echo "$BIN not found" ; exit 1; )
-
+[ $? -ne 0 ] && ( echo "$OPENSCAD not found" ; exit 1; )
 echo $VER
 
+# input is stdin
 while read ; do
 
+	# number and name by line
 	[[ $REPLY =~ ^([0-9]+)[\\t\ ](.+)$ ]]
 	NUM=${BASH_REMATCH[1]}
         NAME=${BASH_REMATCH[2]}
 
+	# output file
 	OUT="$DIR/$NUM.stl"
+
+	# when not exists
 	if [ ! -f $OUT ] ; then
 
+		# generate new stl
 		echo "Generating $OUT"
 		$OPENSCAD -o "$OUT" -D num="\"$NUM\"" -D name="\"$NAME\"" "$SRC"
 
--- src/cronjob.sh
+++ src/cronjob.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-export OPENSCAD=/usr/bin/openscad
-export HOME=/home/caligari
-GIT=/usr/bin/git
-
-. $HOME/.ssh/agent/adn-cw.ssh-id
-
+# paths
 BASEDIR=$(dirname $0)
-cd $BASEDIR
+GIT="/usr/bin/git -C $BASEDIR"
 
+# git+ssh environment
+export HOME=/home/caligari
+source $HOME/.ssh/agent/adn-cw.ssh-id
+
+# update
 $GIT pull 
+
+# generate
 $BASEDIR/adn-cw.sh < $BASEDIR/adn-cw.list 
+
+# send changes
 $GIT add $BASEDIR/../*.stl 
-$GIT commit -m "Partial generation." ; 
+$GIT commit -m "Partial generation."
 $GIT push 
